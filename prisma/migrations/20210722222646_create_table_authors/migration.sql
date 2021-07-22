62bea36026fd0f4c5c3fc2019fcd55bcf169cfb0
Abel Souza <abelsouzacosta@gmail.com>
2021-07-22 19:27:18 -0300

    feat(authors): adicionando table de migrations de autores

---
+++ prisma/migrations/20210722222646_create_table_authors/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Author" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "date_birth" TIMESTAMP(3) NOT NULL,
+    "description" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '.prisma/client';
 
-const client = PrismaClient();
+const client = new PrismaClient();
 
 export { client };
