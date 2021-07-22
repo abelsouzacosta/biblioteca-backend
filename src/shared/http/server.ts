711a42f0bedae094c3d2b4c55bb58060ce661951
Abel Souza <abelsouzacosta@gmail.com>
2021-07-22 19:05:40 -0300

    feat(server): adicionando middleware de erros
    Este middleware permite escrever controllers e services mais limpos de modo que todo o tratamento de
    erros da aplicação seja feita pela camada mais superior

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'express-async-errors';
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './routes';
+import ApplicationError from '@errors/ApplicationError';
 
 const app = express();
 
@@ -10,5 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
+
+// middleware de errors
+app.use(
+  // eslint-disable-next-line
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApplicationError)
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+
+    return response.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
+  },
+);
 
 app.listen(process.env.PORT);
