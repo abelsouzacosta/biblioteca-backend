71a272b74e1c4b58b6488d8676747870f63c146c
Abel Souza <abelsouzacosta@gmail.com>
2021-07-22 18:57:14 -0300

    feat(server): adicionando rotas da aplicação

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,10 +2,13 @@ import 'express-async-errors';
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { router } from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 app.listen(process.env.PORT);
