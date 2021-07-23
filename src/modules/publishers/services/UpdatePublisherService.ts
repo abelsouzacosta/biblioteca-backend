2d08717ee688f60cd65c05df7dbb80eb76b905be
Abel Souza <abelsouzacosta@gmail.com>
2021-07-23 00:27:36 -0300

    chore(editora): atualização da instância de uma editora
    Adicionando serviço que permite atualizar informações sobre uma editora

---
+++ src/modules/publishers/services/UpdatePublisherService.ts
@@ -0,0 +1,44 @@
+import ApplicationError from '@errors/ApplicationError';
+import { client } from '@databaseClient/client';
+import { IPublisherResponseOrUpdate } from '../interfaces/IPublisherResponseOrUpdate';
+
+export default class UpdatePublisherService {
+  public async execute({
+    id,
+    name,
+  }: IPublisherResponseOrUpdate): Promise<
+    IPublisherResponseOrUpdate | undefined
+  > {
+    // busca a editora pelo nome
+    const getPublisherById = await client.publisher.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!getPublisherById) throw new ApplicationError('Publisher not found');
+
+    // busca a editora pelo nome
+    const getPublisherByName = await client.publisher.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    // se os ids da editora encontrada pelo nome e encontrada pelo id
+    // forem diferentes significa que há uma tentativa de duplicação
+    if (getPublisherById.id !== getPublisherByName?.id)
+      throw new ApplicationError('Could not update, duplicate entry');
+
+    const publisher = await client.publisher.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return publisher;
+  }
+}
